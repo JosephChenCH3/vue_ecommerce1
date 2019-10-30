c52c585ac050d10b2dee040a9cecc17f0f7b2a34
Joseph Chen <satan.ch3@gmail.com>
2019-10-30 23:56:23 +0800

    dist test update

---
+++ dist/precache-manifest.92aba99d50fbea4218578ea55f3ea29c.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "58c2bdc4b280b4b0cc0ed8c18db02bc9",
+    "url": "/vue_ecommerce1/img/43353_560_191015_TW2_chin.58c2bdc4.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue_ecommerce1/robots.txt"
+  },
+  {
+    "revision": "4088630b469cc6f2b2d0dc826441b765",
+    "url": "/vue_ecommerce1/img/43492_560_191018_TW_chin.4088630b.jpg"
+  },
+  {
+    "revision": "b5e0a16dfca52422e642",
+    "url": "/vue_ecommerce1/js/chunk-03371d24.98835388.js"
+  },
+  {
+    "revision": "d1a935eceeff7eb298d4",
+    "url": "/vue_ecommerce1/js/chunk-1fadf692.64363d60.js"
+  },
+  {
+    "revision": "b68e299c5615e476dc15",
+    "url": "/vue_ecommerce1/js/chunk-1fb005d0.5b11b352.js"
+  },
+  {
+    "revision": "684f3ba73c677f46591e",
+    "url": "/vue_ecommerce1/js/chunk-1fc43ea1.0fba93d5.js"
+  },
+  {
+    "revision": "5af99463c5772d23b2f0",
+    "url": "/vue_ecommerce1/js/chunk-1fc520f5.bf50202a.js"
+  },
+  {
+    "revision": "b5d22b2584bce1794bcf",
+    "url": "/vue_ecommerce1/js/chunk-21a30340.15ce602b.js"
+  },
+  {
+    "revision": "8da87972a4e32ec8d0c5",
+    "url": "/vue_ecommerce1/js/chunk-2d0cfdeb.5f70daf4.js"
+  },
+  {
+    "revision": "0d611e0d18236c61b99d",
+    "url": "/vue_ecommerce1/js/chunk-2d0e88dd.5d4a3447.js"
+  },
+  {
+    "revision": "7d70d4aab0064792df57",
+    "url": "/vue_ecommerce1/js/chunk-06e4cc76.fa08b0ae.js"
+  },
+  {
+    "revision": "48ba6e8a6a8378a4cefe",
+    "url": "/vue_ecommerce1/js/chunk-3912caf4.ddb9be70.js"
+  },
+  {
+    "revision": "1e95c9dd8094aa17cb9b",
+    "url": "/vue_ecommerce1/js/chunk-8313a57e.69f4019d.js"
+  },
+  {
+    "revision": "04f36637551782f4738c",
+    "url": "/vue_ecommerce1/js/chunk-vendors.3c8c4276.js"
+  },
+  {
+    "revision": "91e5f73b60ac715f3a4e",
+    "url": "/vue_ecommerce1/js/chunk-9aab615a.20ed74ba.js"
+  },
+  {
+    "revision": "a68658de838413b0326b",
+    "url": "/vue_ecommerce1/js/chunk-fd8c8b6a.43782150.js"
+  },
+  {
+    "revision": "1e65a3308abac14118d9",
+    "url": "/vue_ecommerce1/js/chunk-a5338efe.e9061a13.js"
+  },
+  {
+    "revision": "7b5bc94a95880e26ff28",
+    "url": "/vue_ecommerce1/js/chunk-c86735e6.5195296f.js"
+  },
+  {
+    "revision": "a68f1b043f499b0a3414",
+    "url": "/vue_ecommerce1/js/chunk-b06956a8.d7f94ca2.js"
+  },
+  {
+    "revision": "2ef98039f357675175749a9cbbdaf1e5",
+    "url": "/vue_ecommerce1/img/logo2.2ef98039.png"
+  },
+  {
+    "revision": "7b5bc94a95880e26ff28",
+    "url": "/vue_ecommerce1/css/chunk-c86735e6.51eb2784.css"
+  },
+  {
+    "revision": "fc8d53e80ffa5105db569f15cf722676",
+    "url": "/vue_ecommerce1/index.html"
+  },
+  {
+    "revision": "04f36637551782f4738c",
+    "url": "/vue_ecommerce1/css/chunk-vendors.472c2b9a.css"
+  },
+  {
+    "revision": "ca2a96c2fd7e3c7999f4158d58150982",
+    "url": "/vue_ecommerce1/img/Coupon-High-Quality-PNG.ca2a96c2.png"
+  },
+  {
+    "revision": "9f1cd63ac515b80e6dc0",
+    "url": "/vue_ecommerce1/js/app.134171ad.js"
+  },
+  {
+    "revision": "4c0321805e48b242c854ba283d47eb58",
+    "url": "/vue_ecommerce1/img/43536_560_191004_TW01.4c032180.jpg"
+  },
+  {
+    "revision": "60c1adfc0f6b98f481843538ce14af19",
+    "url": "/vue_ecommerce1/img/sports.60c1adfc.jpg"
+  },
+  {
+    "revision": "4f67a54acad3e9b25debad60cbf8a363",
+    "url": "/vue_ecommerce1/img/women.4f67a54a.jpg"
+  },
+  {
+    "revision": "7b2e8504e5228a9858b6b82d92e6c370",
+    "url": "/vue_ecommerce1/img/43377_560_190814_TW_0_chin.7b2e8504.jpg"
+  },
+  {
+    "revision": "d13a28cf2b3ae921f51f167523e1eef5",
+    "url": "/vue_ecommerce1/img/baby.d13a28cf.jpg"
+  },
+  {
+    "revision": "7d4a89340c54cb7a1ea7e5341260a08b",
+    "url": "/vue_ecommerce1/img/43514_560_191019_TW_chin.7d4a8934.jpg"
+  },
+  {
+    "revision": "164549975929284ddbf89cf81a1b9054",
+    "url": "/vue_ecommerce1/img/slide05.16454997.jpg"
+  },
+  {
+    "revision": "db3ca1f990f52e4b32339e87e36e615e",
+    "url": "/vue_ecommerce1/img/slide02.db3ca1f9.jpg"
+  },
+  {
+    "revision": "e83dcd075026244716e70410bbc2f102",
+    "url": "/vue_ecommerce1/img/NEWS_560_191003_TW.e83dcd07.jpg"
+  },
+  {
+    "revision": "7acfb431ac553ecf23bf337aabce43ee",
+    "url": "/vue_ecommerce1/img/promo.7acfb431.jpg"
+  },
+  {
+    "revision": "0869a680eee17c50a2070516703aa23a",
+    "url": "/vue_ecommerce1/img/search.0869a680.jpg"
+  },
+  {
+    "revision": "31b1e610125086b99d11b91430a7fe83",
+    "url": "/vue_ecommerce1/img/kids.31b1e610.jpg"
+  },
+  {
+    "revision": "bc40541d6b6e19445ede391684b65cba",
+    "url": "/vue_ecommerce1/img/slide04.bc40541d.jpg"
+  },
+  {
+    "revision": "abe5e10b627190d6b9136b90aece9cb2",
+    "url": "/vue_ecommerce1/img/43640_560_191004_TW2.abe5e10b.jpg"
+  },
+  {
+    "revision": "060cf4882f3ab85eca96f46aa9d60cb1",
+    "url": "/vue_ecommerce1/img/43455_560_191003_TW03.060cf488.jpg"
+  },
+  {
+    "revision": "f582b7a95bca079cccf7c99e2e05a41f",
+    "url": "/vue_ecommerce1/img/FS680_560_191004_TW_1.f582b7a9.jpg"
+  },
+  {
+    "revision": "ce5929b4066f979402847a5b3fb86ff7",
+    "url": "/vue_ecommerce1/img/slide03.ce5929b4.jpg"
+  },
+  {
+    "revision": "b92590756f068bd8f8db95b868ea220c",
+    "url": "/vue_ecommerce1/img/men.b9259075.jpg"
+  },
+  {
+    "revision": "44c79bcb024a14c363b9dee342b05a8d",
+    "url": "/vue_ecommerce1/img/slide01.44c79bcb.jpg"
+  },
+  {
+    "revision": "a68f1b043f499b0a3414",
+    "url": "/vue_ecommerce1/css/chunk-b06956a8.e9476e8a.css"
+  },
+  {
+    "revision": "1e65a3308abac14118d9",
+    "url": "/vue_ecommerce1/css/chunk-a5338efe.227cf134.css"
+  },
+  {
+    "revision": "91e5f73b60ac715f3a4e",
+    "url": "/vue_ecommerce1/css/chunk-9aab615a.bdb28d1d.css"
+  },
+  {
+    "revision": "48ba6e8a6a8378a4cefe",
+    "url": "/vue_ecommerce1/css/chunk-3912caf4.4773dde2.css"
+  },
+  {
+    "revision": "9f1cd63ac515b80e6dc0",
+    "url": "/vue_ecommerce1/css/app.fb9e19c7.css"
+  }
+];
